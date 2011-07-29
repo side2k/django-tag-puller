f03746e978d20db4d7b98e0b33a2b516bf9dfcbb
outsider <siderelay@gmail.com>
2011-07-29 06:48:14 +0400

    given name to an url 'tag-puller-link'

--- tag_puller/urls.py
+++ tag_puller/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import *
 from tag_puller.views import pull
 
 urlpatterns = patterns('',
-    url(r'(?P<tagnames>[\w-]+)/', pull),
+    url(r'(?P<tagnames>[\w-]+)/', pull, name='tag-puller-link'),
 )
 
